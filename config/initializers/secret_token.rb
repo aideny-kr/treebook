6a77fd52ee723167b7dffe09f89a4a0477990c2e
Huichan Yi <huichanyi@gmail.com>
2013-07-23 11:11:11 -0400

    Created rails application and added stutus file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '7df7acddf64f772c4cce6a49f66d93f04ccba7397d3c2b8563d2ccd72a885f2fdbb263df1aac657d83c2e6e734c67350cf59d138c4761ed601b33f7392f6c2b4'
