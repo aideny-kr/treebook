aa2c558b417c9feda8acbe155c103fcebc4d8085
Chan Yi <huichanyi@gmail.com>
2013-10-07 16:05:15 -0400

    Wrote test for profile_name, first_name, last_name and password correctness

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "a user should enter a first name" do
+		user = User.new
+		assert !user.save #user should not be saved in database
+		assert !user.errors[:first_name].empty? #Asserting that the errors on the first name field is not empty
+	end
+
+	test "a user should enter a last name" do
+		user = User.new
+		assert !user.save #user should not be saved in database
+		assert !user.errors[:last_name].empty? #Asserting that the errors on the first name field is not empty
+	end
+
+	test "a user should enter a profile name" do
+		user = User.new
+		assert !user.save #user should not be saved in database
+		assert !user.errors[:profile_name].empty? #Asserting that the errors on the first name field is not empty
+	end
+
+	test "a user should have a unique profile name" do
+		user = User.new
+
+		user.profile_name = users(:huichanyi).profile_name
+	
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a profile name without spaces" do
+		user = User.new
+
+		user.profile_name = "Huichan Yi"
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Must be formatted correctly")
+	end
+
+
+
 end
