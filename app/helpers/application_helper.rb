765cccd264aee80ade532945f59a99b24f76d590
Chan Yi <huichanyi@gmail.com>
2013-10-21 16:16:12 -0400

    Added custom route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def flash_class(type)
+		case type
+			when type = :alert
+				"alert-info"
+			when type = :notice
+				"alert-success"
+			else
+				""
+		end
+
+	end
 end
